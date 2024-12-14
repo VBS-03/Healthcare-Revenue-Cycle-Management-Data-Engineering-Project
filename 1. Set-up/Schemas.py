a68fbba1e479238685ea9f3b6c80769d7d89aaf7
Vivek Bhaskar Sharma <162421729+VBS-03@users.noreply.github.com>
2024-12-14 20:05:06 +0530

    Delete 1. Set-up directory

--- 1. Set-up/Schemas.py
+++
@@ -1,8 +0,0 @@
-# Databricks notebook source
-# MAGIC %sql
-# MAGIC CREATE SCHEMA IF NOT EXISTS healthcare_rcm_catalog.silver;
-
-# COMMAND ----------
-
-# MAGIC %sql
-# MAGIC CREATE SCHEMA IF NOT EXISTS healthcare_rcm_catalog.gold;
